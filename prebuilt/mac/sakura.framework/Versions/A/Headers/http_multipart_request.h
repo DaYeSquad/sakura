5cb9041a2184a5378363b0e4541c1d4a968d301c
FrankLin <lin.xiaoe.f@gmail.com>
2015-12-30 11:16:04 +0800

    Add iOS and mac prebuilt

---
+++ prebuilt/mac/sakura.framework/Versions/A/Headers/http_multipart_request.h
@@ -0,0 +1,54 @@
+//
+//  http_multipart_request.hpp
+//  WorktileCore
+//
+//  Created by Frank Lin on 11/10/15.
+//  Copyright © 2015 Frank Lin. All rights reserved.
+//
+
+#ifndef SAKURA_HTTP_MULTIPART_REQUEST_H_
+#define SAKURA_HTTP_MULTIPART_REQUEST_H_
+
+#include <string>
+#include <vector>
+#include <map>
+#include <functional>
+#include <memory>
+
+#include "sakura/macros.h"
+#include "sakura/http_request.h"
+
+NS_SKR_BEGIN
+
+/// Used for multipart request to PUT or POST.
+class HttpMultipartRequest: public sakura::HttpRequest {
+public:
+  
+  // Creation and lifetime --------------------------------------------------------
+  
+  HttpMultipartRequest();
+  
+  virtual ~HttpMultipartRequest() {}
+  
+  // Multipart --------------------------------------------------------
+  
+  // adds string body
+  void AddPart(const std::string& name, const std::string& value);
+  
+  // adds binary file body
+  void AddFileBody(const std::string& name,
+                   const std::string& file_name,
+                   const std::string& file_type,
+                   const std::vector<char>& file_data);
+  
+  // Getter --------------------------------------------------------
+  
+  std::vector<char> raw_data() const { return raw_data_; }
+  
+private:
+  DISALLOW_COPY_AND_ASSIGN(HttpMultipartRequest);
+};
+
+NS_SKR_END
+
+#endif /* http_multipart_request_hpp */
---
+++ prebuilt/mac/sakura.framework/Versions/A/Headers/log.h
@@ -0,0 +1,24 @@
+﻿//
+//  log.h
+//  LessChatCore
+//
+//  Created by Frank Lin on 11/19/14.
+//
+//
+
+#ifndef SAKURA_LOG_H_
+#define SAKURA_LOG_H_
+
+#include "sakura/macros.h"
+
+NS_SKR_BEGIN
+
+static const int kMaxLogLen = 16 * 1024;
+
+SKR_DLL void log_event(const char*format, ...);
+SKR_DLL void log_warning(const char*format, ...);
+SKR_DLL void log_error(const char*format, ...);
+
+NS_SKR_END
+
+#endif /* defined(SAKURA_LOG_H_) */
