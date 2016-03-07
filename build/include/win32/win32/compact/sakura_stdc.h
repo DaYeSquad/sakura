1cc7243e54bd63f214b73485a0263b27bf5f1803
Frank Lin <lin.xiaoe.f@gmail.com>
2016-03-08 06:48:37 +0800

    Remove websockets related class from sakura win32 build.

---
+++ build/include/win32/win32/compact/sakura_stdc.h
@@ -0,0 +1,12 @@
+#ifndef SAKURA_SAKURA_STDC_H_
+#define SAKURA_SAKURA_STDC_H_
+
+#include <BaseTsd.h>
+#ifndef __SSIZE_T
+#define __SSIZE_T
+typedef SSIZE_T ssize_t;
+#endif // __SSIZE_T
+
+#undef min
+
+#endif
